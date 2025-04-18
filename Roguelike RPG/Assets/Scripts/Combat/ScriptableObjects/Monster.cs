d4e05685fdd053ebff7eac6a27ff77b46688a84b
Iván Campos <ivancampos.professional@gmail.com>
2025-04-18 16:48:41 +0200

    Monster Getters & Setters and Check Level Up function

--- Roguelike RPG/Assets/Scripts/Combat/ScriptableObjects/Monster.cs
+++ Roguelike RPG/Assets/Scripts/Combat/ScriptableObjects/Monster.cs
@@ -19,13 +19,13 @@ public class Monster : ScriptableObject
     [Header("Basic Data")]
     //Create all the variables of the monster scriptable object and serialize it all to change it from the object inspector
     [SerializeField] private string monsterName;
-    [SerializeField] private string description;
+    [SerializeField] private string monsterDescription;
     //Si tengo problemas al trabajar con esta variable puedo cambiarla a un String
     //Para operar con ella, por ejemplo checkear si un monster es de un tipo, puedo hacer "if(Monster.Type == MonsterType.Flesh)"
-    [SerializeField] MonsterType type;
+    [SerializeField] MonsterType monsterType;
     [SerializeField] private Sprite monsterIcon;
     [SerializeField] private Sprite monsterSprite;
-    [SerializeField] private PassiveAbility passiveAbility;
+    [SerializeField] private PassiveAbility monsterPassiveAbility;
 
     [Space]
     [Header("Stats")]
@@ -49,7 +49,7 @@ public class Monster : ScriptableObject
     [Header("Object & Debuffs")]
     [SerializeField] private Item equipableObject;
     [SerializeField] private string debuff;
-    [SerializeField] private string debuffCounter;
+    [SerializeField] private int debuffCounter;
 
     [Serializable]
     public struct MaterialToCraftQuantityPair{
@@ -58,7 +58,7 @@ public class Monster : ScriptableObject
     }
     [Space]
     [Header("Crafting System")]
-    [SerializeField] private List<MaterialToCraftQuantityPair> MaterialAndQuantityToCraftList = new List<MaterialToCraftQuantityPair>();
+    [SerializeField] private List<MaterialToCraftQuantityPair> materialAndQuantityToCraftList = new List<MaterialToCraftQuantityPair>();
 
     [Space]
     [Header("Level System")]
@@ -87,15 +87,123 @@ public class Monster : ScriptableObject
         public int quanity;
     }
     //Create a list with the structure of material and quantity
-    [SerializeField] private List<MaterialToEvolveQuantityPair> MaterialAndQuantityToEvolveList = new List<MaterialToEvolveQuantityPair>();
-    [SerializeField] private string monsterEvolution;
+    [SerializeField] private List<MaterialToEvolveQuantityPair> materialAndQuantityToEvolveList = new List<MaterialToEvolveQuantityPair>();
+    [SerializeField] private Monster monsterEvolution;
 
     [Space]
-    [Header("Normal Attacks & Special Attacks")]
+    [Header("Attacks")]
     //The attacks list needs to be defined with the size of 10 to know by design how much attacs we want to set on a monster
-    [SerializeField] private Attack[] attackList = new Attack[10];
-    [SerializeField] private Attack[] specialAttackList = new Attack[6];
+    [SerializeField] private Attack[] attacksList = new Attack[10];
+    [SerializeField] private Attack[] specialAttacksList = new Attack[6];
 
+    //GETTERS & SETTERS BASIC DATA
+    public string MonsterName{
+        get { return monsterName; }
+        set { monsterName = value; }
+    }
+
+    public string MonsterDescription{
+        get { return monsterDescription; }
+        set { monsterDescription = value; }
+    }
+
+    public MonsterType MonsterType{
+        get { return monsterType; }
+        set { monsterType = value; }
+    }
+
+    public Sprite MonsterIcon{
+        get { return monsterIcon; }
+        set { monsterIcon = value; }
+    }
+
+    public Sprite MonsterSprite{
+        get { return monsterSprite; }
+        set { monsterSprite = value; }
+    }
+
+    public PassiveAbility MonsterPassiveAbility{
+        get { return monsterPassiveAbility; }
+        set { monsterPassiveAbility = value; }
+    }
+
+    //GETTERS & SETTERS STATS
+    public int HealthPoints{
+        get { return healthPoints; }
+        set { healthPoints = value; }
+    }
+
+    public int ManaPoints{
+        get { return manaPoints; }
+        set { manaPoints = value; }
+    }
+
+    public int SpecialAttackCharges{
+        get { return specialAttackCharges; }
+        set { specialAttackCharges = value; }
+    }
+
+    public int Damage{
+        get { return damage; }
+        set { damage = value; }
+    }
+
+    public int ArcaneDamage{
+        get { return arcaneDamage; }
+        set { arcaneDamage = value; }
+    }
+
+    public int Defense{
+        get { return defense; }
+        set { defense = value; }
+    }
+
+    public int ArcaneDefense{
+        get { return arcaneDefense; }
+        set { arcaneDefense = value; }
+    }
+
+    public int Velocity{
+        get { return velocity; }
+        set { velocity = value; }
+    }
+
+    public int HealthPointsIVs{
+        get { return healthPointsIVs; }
+        set { healthPointsIVs = value; }
+    }
+
+    public int ManaPointsIVs{
+        get { return manaPointsIVs; }
+        set { manaPointsIVs = value; }
+    }
+
+    public int DamageIVs{
+        get { return damageIVs; }
+        set { damageIVs = value; }
+    }
+
+    public int ArcaneDamageIVs{
+        get { return arcaneDamageIVs; }
+        set { arcaneDamageIVs = value; }
+    }
+
+    public int DefenseIVs{
+        get { return defenseIVs; }
+        set { defenseIVs = value; }
+    }
+
+    public int ArcaneDefenseIVs{
+        get { return arcaneDefenseIVs; }
+        set { arcaneDefenseIVs = value; }
+    }
+
+    public int VelocityIVs{
+        get { return velocityIVs; }
+        set { velocityIVs = value; }
+    }
+
+    //GETTERS & SETTERS OBJECT & DEBUFFS
     public Item EquipableObject{
         get { return equipableObject; }
         //The type of the equipable object you want to add to the monster has to be equipable object type
@@ -104,6 +212,62 @@ public class Monster : ScriptableObject
                 equipableObject = value;
            } 
         }
+    }
+
+    public string Debuff{
+        get { return debuff; }
+        set { debuff = value; }
+    }
+
+    public int DebuffCounter{
+        get { return debuffCounter; }
+        set { debuffCounter = value; }
+    }
+
+    //GETTERS & SETTERS CRAFTING SYSTEM
+    public List<MaterialToCraftQuantityPair> MaterialAndQuantityToCraftList
+    {
+        get { return materialAndQuantityToCraftList; }
+        set { materialAndQuantityToCraftList = value; }
+    }
+
+    //Create a Getter to know the materials i need to craft the monster, later you can access the material name or anything you need from the list
+    public List<Item> GetMaterialsToCraft(){
+        List<Item> materials = new List<Item>();
+        foreach(var pair in materialAndQuantityToCraftList){
+            materials.Add(pair.materialToCraft);
+        }
+
+        return materials;
+    }
+
+    //Create a Getter to know the quantity of a material you need to evolve
+    public int GetQuantityOfAMaterialNeededToCraft(string materialName){
+        foreach(var pair in materialAndQuantityToCraftList){
+            if(pair.materialToCraft.ItemName == materialName){
+                return pair.quanity;
+            }
+        }
+
+        Debug.LogWarning($"Material {materialName} not needed to evolve.");
+        return -1;
+    }
+
+    //GETTERS & SETTERS LEVEL SYSTEM
+    public int CurrentLevel{
+        get { return currentLevel; }
+        set { currentLevel = value; }
+    }
+
+    public int CurrentExp{
+        get { return currentExp; }
+        set { currentExp = value; }
+    }
+
+    public List<LevelExperiencePair> LevelSystem
+    {
+        get { return levelSystem; }
+        set { levelSystem = value; }
     }
 
     //Create a Getter to know the experience that the monster needs to Level Up passing it the level you need to know
@@ -118,10 +282,22 @@ public class Monster : ScriptableObject
         return -1;
     }
 
+    //GETTERS & SETTERS EVOLVUTION SYSTEM
+    public int MinimumLevelToEvolve{
+        get { return minimumLevelToEvolve; }
+        set { minimumLevelToEvolve = value; }
+    }
+
+    public List<MaterialToEvolveQuantityPair> MaterialAndQuantityToEvolveList
+    {
+        get { return materialAndQuantityToEvolveList; }
+        set { materialAndQuantityToEvolveList = value; }
+    }
+
     //Create a Getter to know the materials i need to evolve, later you can access the material name or anything you need from the list
     public List<Item> GetMaterialsToEvolve(){
         List<Item> materials = new List<Item>();
-        foreach(var pair in MaterialAndQuantityToEvolveList){
+        foreach(var pair in materialAndQuantityToEvolveList){
             materials.Add(pair.materialToEvolve);
         }
 
@@ -130,7 +306,7 @@ public class Monster : ScriptableObject
 
     //Create a Getter to know the quantity of a material you need to evolve
     public int GetQuantityOfAMaterialNeededToEvolve(string materialName){
-        foreach(var pair in MaterialAndQuantityToEvolveList){
+        foreach(var pair in materialAndQuantityToEvolveList){
             if(pair.materialToEvolve.ItemName == materialName){
                 return pair.quanity;
             }
@@ -140,25 +316,31 @@ public class Monster : ScriptableObject
         return -1;
     }
 
-    //Create a Getter to know the materials i need to craft the monster, later you can access the material name or anything you need from the list
-    public List<Item> GetMaterialsToCraft(){
-        List<Item> materials = new List<Item>();
-        foreach(var pair in MaterialAndQuantityToCraftList){
-            materials.Add(pair.materialToCraft);
-        }
-
-        return materials;
+    public Monster MonsterEvolution{
+        get { return monsterEvolution; }
+        set { monsterEvolution = value; }
     }
 
-    //Create a Getter to know the quantity of a material you need to evolve
-    public int GetQuantityOfAMaterialNeededToCraft(string materialName){
-        foreach(var pair in MaterialAndQuantityToCraftList){
-            if(pair.materialToCraft.ItemName == materialName){
-                return pair.quanity;
-            }
-        }
+    //GETTERS & SETTERS ATTACKS
+    public Attack[] AttacksList{
+        get { return attacksList; }
+        set { attacksList = value; }
+    }
 
-        Debug.LogWarning($"Material {materialName} not needed to evolve.");
-        return -1;
+    public Attack[] SpecialAttacksList{
+        get { return specialAttacksList; }
+        set { specialAttacksList = value; }
+    }
+
+    //OTHER FUNCTIONS
+    //Function to check if the monster leveled up
+    public bool CheckLevelUp(){
+        if(currentExp >= GetExperienceToLevelUp(currentLevel)){
+            currentLevel++;
+            currentExp -= GetExperienceToLevelUp(currentLevel);
+            return true;
+        }else{
+            return false;
+        }
     }
 }
